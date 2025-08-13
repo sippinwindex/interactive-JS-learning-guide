bf9eb4595fb2669e75097e70a59e5bc4b4a896a6
Juan Fernandez <stormblazart@gmail.com>
2025-08-13 04:27:04 +0000

    toast2

--- src/front/providers/ToastProvider.jsx
+++ src/front/providers/ToastProvider.jsx
@@ -1,6 +1,6 @@
 // src/front/providers/ToastProvider.jsx
 import React, { createContext, useContext, useState, useCallback } from 'react';
-import Toast from "../components/Toast";
+import Toast from "../components/ui/Toast";
 
 const ToastContext = createContext();
 
