7808e4ca46ae291f9c4dd65d2f589a4e095e7b7d
Juan Fernandez <stormblazart@gmail.com>
2025-08-13 04:31:38 +0000

    icons4

--- src/front/components/Toast.jsx
+++ src/front/components/Toast.jsx
@@ -1,6 +1,6 @@
 // src/front/components/ui/Toast.jsx
 import React, { useState, useEffect } from 'react';
-import { XIcon, InfoIcon, ExclamationTriangleIcon, CheckCircleIcon } from './ui/Icons';
+import { XIcon, InformationCircleIcon, ExclamationTriangleIcon, CheckCircleIcon } from './ui/Icons';
 
 export const Toast = ({ 
   show, 
